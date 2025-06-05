094c93111fb7bdb8c082679680155e4ddcca9382
Deepakpd811 <deepak0413.be21@chitkara.edu.in>
2025-06-05 14:37:07 +0530

    feat: add stater pack

---
+++ src/main/java/com/bridgelab/warmup_project/WarmupProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelab.warmup_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarmupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarmupProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelab/warmup_project/WarmupProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelab.warmup_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarmupProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
